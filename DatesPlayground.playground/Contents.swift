e899e0832aa8224521bda8e9edebfd5dcb0b3f91
Rayan Ahmed <rahmed@Roberts-Mac-mini.local>
2019-10-30 22:16:52 -0500

    update date homework

--- DatesPlayground.playground/Contents.swift
+++ DatesPlayground.playground/Contents.swift
@@ -1,16 +1,28 @@
 import Foundation
 
-// Start with a date, like so:
+var currDate = Date()
 
-let date = Date()
 
 // Create a date using today's date and a DateComponents structure
+
+// Specify date components
+var dateComponents = DateComponents()
+
+dateComponents.year = 2019
+dateComponents.month = 10
+dateComponents.day = 30
+dateComponents.hour = 8
+dateComponents.minute = 34
+
+// Create date from components
+let userCalendar = Calendar.current // user calendar
+let someDateTime = userCalendar.date(from: dateComponents)
+print("The dates are equal: \(currDate == someDateTime)")
 
 // Compare the two dates to see if they occur in the same Calendar day
 
 // Use a date formatter
-
 let formatter = DateFormatter()
-
-// use the formatter to print the day in a friendly format.
-// hint, use NSDateFormatter.com to get an appropriate printing format string.
+formatter.dateFormat = "yyyy/MM/dd HH:mm"
+let dateFromFormatter = formatter.date(from: "2019/10/30 22:31")
+print(dateFromFormatter!)
